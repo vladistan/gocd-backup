095698fc55f8b1315547c2653c6d2c68e73806b2
Vlad <vlad@v-lad.org>
2017-03-10 09:50:15 -0500

    Add backup script

---
+++ ci/backup-gocd.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -k "https://${GO_MASTER_HOST}:${GO_MASTER_PORT}/go/api/backups" \
+      -u "${BACKUP_USER}:${BACKUP_PASSWORD}" \
+      -H 'Confirm: true' \
+      -H 'Accept: application/vnd.go.cd.v1+json' \
+      -X POST
